bb48e68c5aee88f49b14abc058e0a7a5572c8ea3
Rickie Kerndt <kerndtr@kerndt.com>
2016-08-01 10:22:45 -0700

    use imp.load_source and add some color

--- display_ip_address.py
+++ display_ip_address.py
@@ -5,7 +5,10 @@ Rickie Kerndt <rkerndt@cs.uoregon.edu>
 
 from __future__ import print_function
 
-SEARCH_PATH = '/home/pi/GrovePi/Software/Python'
+UO_GREEN = (0,79,39)
+UO_YELLOW = (255,204,0)
+
+MODULE_PATH = '/home/pi/GrovePi/Software/Python/grove_rgb_lcd/grove_rgb_lcd.py'
 MODULE_NAME = 'grove_rgb_lcd'
 ADDR_STR = 'new_ip_address'
 
@@ -18,13 +21,12 @@ from sys import stderr
 import imp
 
 try:
-    filename, pathname, description = imp.find_module(MODULE_NAME,SEARCH_PATH)
+    grove_rgb_lcd = imp.load_source(MODULE_NAME, MODULE_PATH)
 except ImportError:
     msg = 'display_ip_address: failed to find %s module' % MODULE_NAME
     print(msg, file=stderr)
     exit(1)
 
-grove_lcd = imp.load_module(filename, pathname, description)
-
-grove_lcd.setText(environ[ADDR_STR])
+grove_rgb_lcd.setRGB(*UO_GREEN)
+grove_rgb_lcd.setText(environ[ADDR_STR])
 exit(1)
